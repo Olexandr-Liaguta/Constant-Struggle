094257134604658abcf0737c1247a43276c38e14
Olexandr-Liaguta <olexandr.liaguta@gmail.com>
2023-01-03 13:26:04 +0200

    [Landmass Generation]: E03: octaves

--- Assets/Scripts/LandmassGeneration/MapGenerator.cs
+++ Assets/Scripts/LandmassGeneration/MapGenerator.cs
@@ -7,13 +7,53 @@ public class MapGenerator : MonoBehaviour
     public int mapWidth, mapHeight;
     public float noiseScale;
 
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);    
+        float[,] noiseMap = Noise.GenerateNoiseMap(
+            mapWidth: mapWidth,
+            mapHeight: mapHeight,
+            octaves: octaves,
+            scale: noiseScale,
+            persistance: persistance,
+            lacunarity: lacunarity,
+            seed: seed,
+            offset: offset
+            );
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
     }
 }

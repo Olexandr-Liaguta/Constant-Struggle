71e6487421ec319b8723f380252575d4c322e4e1
Olexandr-Liaguta <olexandr.liaguta@gmail.com>
2023-12-25 23:50:58 +0200

    [MAP]: Add GameController and attach to Map cells

---
+++ Assets/ScriptableObjects/GameController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum LocationCellStatus
+{
+    Red,
+    Yellow,
+    Green,
+}
+
+[Serializable]
+public class LocationCell
+{
+    public LocationCellStatus status; 
+    public bool isHome = false;
+    public int score;
+}
+
+[CreateAssetMenu(fileName = "GameController", menuName = "GameController")]
+public class GameController : ScriptableObject
+{
+
+    [Serializable]
+    public class TextureList
+    {
+        public LocationCell[] columns = new LocationCell[3];
+    }
+
+    public TextureList[] locationRows = new TextureList[3];
+}
--- Assets/Scripts/Map/MapCell.cs
+++ Assets/Scripts/Map/MapCell.cs
@@ -6,60 +6,87 @@ using UnityEngine.UI;
 
 public class MapCell : MonoBehaviour
 {
-
-    enum CellStatus
+    enum Columns
     {
-        Red,
-        Yellow,
-        Green,
+        One = 1, Two = 2, Three = 3
     }
 
-    [SerializeField ] private bool isHome = false;
+    [SerializeField] private Columns row;
+    [SerializeField] private Columns column;
 
-    [SerializeField] private CellStatus status;
     [SerializeField] private Button button;
+    [SerializeField] private GameController gameController;
+
+    private LocationCell currentLocation;
 
     private void Start()
     {
-        if (isHome) return;
+        currentLocation = gameController.locationRows[((int)row - 1)].columns[(int)column - 1];
 
+        if (currentLocation.isHome) return;
 
-        if (this.status == CellStatus.Red)
+        UpdateButtonColor();
+    }
+
+    private void UpdateButtonColor()
+    {
+        LocationCellStatus status = currentLocation.status;
+
+        if (status == LocationCellStatus.Red)
         {
-            this.SetButtonColors(new Color(0.9f, 0, 0, 1));
+            this.SetButtonColors(new Color(0.8f, 0, 0, 1));
         }
 
-        if (this.status == CellStatus.Yellow)
+        if (status == LocationCellStatus.Yellow)
         {
             this.SetButtonColors(Color.yellow);
         }
 
-        if (this.status == CellStatus.Green)
+        if (status == LocationCellStatus.Green)
         {
-            this.SetButtonColors(new Color(0, 0.9f, 0, 1));
+            this.SetButtonColors(new Color(0, 0.8f, 0, 1));
         }
-
     }
 
     private void SetButtonColors(Color color)
     {
         ColorBlock colors = button.colors;
         Color highlightValue = new Color(0.1f, 0.1f, 0.1f, 0);
+        Color selectedValue = new Color(0.1f, 0.1f, 0.1f, 0);
+        Color pressedValue = new Color(0.2f, 0.2f, 0.2f, 0);
 
         colors.normalColor = color;
         colors.highlightedColor = color + highlightValue;
+        colors.selectedColor = color - selectedValue;
+        colors.pressedColor = color + pressedValue;
+
 
         button.colors = colors;
     }
 
     public void OnClickCell()
     {
-        if(isHome)
-        {
-            // Load Home scene
-        } else
-        {
-          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+        Debug.Log(currentLocation.score);
+
+        if (currentLocation.isHome) return;
+
+        SceneManager.LoadScene("Main");
+
+        // For Test Change Button color
+        /*       
+         *       if(currentLocation.status == LocationCellStatus.Red)
+                {
+                    currentLocation.status = LocationCellStatus.Green;
+                }
+                else if (currentLocation.status == LocationCellStatus.Yellow)
+                {
+                    currentLocation.status = LocationCellStatus.Red;
+                }
+                else if (currentLocation.status == LocationCellStatus.Green)
+                {
+                    currentLocation.status = LocationCellStatus.Yellow;
+                }
+
+                UpdateButtonColor();*/
     }
 }
